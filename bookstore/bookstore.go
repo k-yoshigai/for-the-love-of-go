8f858006679013ec01958764ba554f2084e0d451
keisuke yoshigai <yoshigai_keisuke@retail-ai.jp>
2022-06-19 16:54:45 +0900

    Add functions to get a book or more

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -1,6 +1,9 @@
 package bookstore
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // Book represents information about a book.
 type Book struct {
@@ -15,4 +18,20 @@ func Buy(b Book) (Book, error) {
 	}
 	b.Copies--
 	return b, nil
+}
+
+func GetAllBooks(catalog map[int]Book) []Book {
+	var result []Book
+	for _, book := range catalog {
+		result = append(result, book)
+	}
+	return result
+}
+
+func GetBook(catalog map[int]Book, id int) (Book, error) {
+	if book, ok := catalog[id]; !ok {
+		return Book{}, fmt.Errorf("ID %d doesn't exist", id)
+	} else {
+		return book, nil
+	}
 }
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -2,7 +2,11 @@ package bookstore_test
 
 import (
 	"bookstore"
+	"sort"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	_ "github.com/google/go-cmp/cmp"
 )
 
 func TestBook(t *testing.T) {
@@ -45,5 +49,49 @@ func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
 	_, err := bookstore.Buy(b)
 	if err == nil {
 		t.Error("want error buying book when zero copies left, but got nil")
+	}
+}
+
+func TestGetAllBooks(t *testing.T) {
+	t.Parallel()
+	catalog := map[int]bookstore.Book{
+		1: {Title: "For the Love of Go"},
+		2: {Title: "The Power of Go: Tools"},
+	}
+	want := []bookstore.Book{
+		{Title: "For the Love of Go"},
+		{Title: "The Power of Go: Tools"},
+	}
+
+	got := bookstore.GetAllBooks(catalog)
+	sort.Slice(got, func(i, j int) bool {
+		return got[i].Title < got[j].Title
+	})
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestGetBook(t *testing.T) {
+	t.Parallel()
+	catalog := map[int]bookstore.Book{
+		1: bookstore.Book{Title: "The power of Go: Tools"},
+		2: bookstore.Book{Title: "For the Love of Go"},
+	}
+	want := bookstore.Book{
+		Title: "For the Love of Go",
+	}
+	got, _ := bookstore.GetBook(catalog, 2)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestGetBookBadIDReturnsError(t *testing.T) {
+	catalog := map[int]bookstore.Book{}
+	_, err := bookstore.GetBook(catalog, 999)
+	if err == nil {
+		t.Fatal("want error for non-existent ID, got nil")
 	}
 }
